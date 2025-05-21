2ff702beb179c8453549f373aa392bfc46c57099
Victor Udnæs <victorud@smint.no>
2025-05-21 10:28:11 +0200

    linked ports with server

--- net_drone_client/net_drone_client/Clients/OperatorClient.cs
+++ net_drone_client/net_drone_client/Clients/OperatorClient.cs
@@ -4,6 +4,8 @@ namespace net_drone_client.Clients;
 
 public class OperatorClient : AbstractNetDroneClient
 {
+    private readonly MovementQueue _movementQueue = new();
+    
     public OperatorClient(int clientPort, int serverPort, string serverIp, int droneId)
     {
         ClientPort = clientPort;
@@ -18,11 +20,17 @@ public class OperatorClient : AbstractNetDroneClient
         _networkClient.SendCommand(command, DroneState.Id);
     }
 
-    public Vec3<float> GetLatestLocationFromDrone()
+    public Vec3<int> GetLatestLocationFromDrone()
     {
         var position = DroneState.Position;
         Console.WriteLine($"Fetching latest location from drone: {position}");
         return position;
+    }
+    
+    public void UpdatePredictedPosition(Vec3<int> predictedPosition)
+    {
+        DroneState.PredictedPosition = predictedPosition;
+        Console.WriteLine($"Predicted position updated to: {predictedPosition}");
     }
     
     protected override void HandleIncomingMessages()
@@ -31,7 +39,7 @@ public class OperatorClient : AbstractNetDroneClient
         {
             Console.WriteLine($"Received location update from drone {message.DroneId}");
             var data = message.Command.Data;
-            DroneState.Position = new Vec3<float>(
+            DroneState.Position = new Vec3<int>(
                 data.X,
                 data.Y,
                 data.Z
