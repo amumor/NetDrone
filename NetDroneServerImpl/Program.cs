3d134d0f3ec9880870327a3acb50bcbd2c3e757b
Amund Mørk <amundmor@stud.ntnu.no>
2025-05-21 10:25:55 +0200

    added simple server impl

---
+++ NetDroneServerImpl/Program.cs
@@ -0,0 +1,14 @@
+﻿using NetDroneServerLib;
+
+internal class Program
+{
+  private static async Task Main(string[] args)
+  {
+    int dronePort = 4001;
+    int clientPort = 4002;
+
+    var server = new NetDroneServer();
+    var cts = new CancellationTokenSource();
+    await server.StartListenersAsync(dronePort, clientPort, cts.Token);
+  }
+}
